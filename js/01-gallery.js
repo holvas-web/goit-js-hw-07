ff3237970b622876774018b387a80ff61daa5fa2
holvas-web <66141432+holvas-web@users.noreply.github.com>
2023-05-04 15:42:50 +0200

    гupdate hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,47 +1,95 @@
-import { galleryItems } from './gallery-items.js'
+import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems)
 // Change code below this line
 
-const gallery = document.querySelector('.gallery')
-const items = []
+// Другий варіант
+const gallery = document.querySelector('.gallery');
+const itemsMarkup = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', itemsMarkup);
+gallery.addEventListener('click', onImgClick);
 
-galleryItems.forEach(element => {
-	const galleryItem = document.createElement('div')
-	galleryItem.className = 'gallery__item'
-	const galleryLink = document.createElement('a')
-	galleryLink.className = 'gallery__link'
-	galleryLink.href = element.original
-	const galleryImage = document.createElement('img')
-    galleryImage.className = 'gallery__image'
-    galleryImage.src = element.preview;
-    galleryImage.setAttribute('data-source', element.original)
-    galleryImage.alt = element.description;
+// візуалізація елементів
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+	  <a class="gallery__link" href="${original}">
+	    <img
+	      class="gallery__image"
+	      src="${preview}"
+	      data-source="${original}"
+	      alt="${description}"
+	    />
+	  </a>
+	</li>`;
+    })
+    .join('');
+}
 
-	galleryItem.append(galleryLink)
-	galleryLink.append(galleryImage)
-	items.push(galleryItem)
-})
+const instance = basicLightbox.create(`<img width="1280" height="auto" src="">`, {
+    onShow: (instance) => {
+	window.addEventListener('keydown', onEscKeyPress);
+    },
+    onClose: (instance) => {
+      window.removeEventListener('keydown', onEscKeyPress);
+    },
+  }
+);
 
-gallery.append(...items)
+function onImgClick(e) {
+  e.preventDefault();
+  const datasetSource = e.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector('img').src = datasetSource;
+  instance.show();
+}
 
-gallery.addEventListener('click', e => {
-    e.preventDefault();
-    if (e.target.nodeName !== 'IMG') {
-		return
-	}
+function onEscKeyPress(e) {
+  if (e.code !== 'Escape') return;
+  instance.close();
+}
 
-    const selectedImage = e.target.getAttribute('data-source')
+// -----Перший варіант-------
+// const gallery = document.querySelector('.gallery')
+// const items = []
 
-    const instance = basicLightbox.create(`
-    <img src="${selectedImage}" width="800" height="600">
-`)
+// galleryItems.forEach(element => {
+// 	const galleryItem = document.createElement('div')
+// 	galleryItem.className = 'gallery__item'
+// 	const galleryLink = document.createElement('a')
+// 	galleryLink.className = 'gallery__link'
+// 	galleryLink.href = element.original
+// 	const galleryImage = document.createElement('img')
+//     galleryImage.className = 'gallery__image'
+//     galleryImage.src = element.preview;
+//     galleryImage.setAttribute('data-source', element.original)
+//     galleryImage.alt = element.description;
 
-    instance.show()
+// 	galleryItem.append(galleryLink)
+// 	galleryLink.append(galleryImage)
+// 	items.push(galleryItem)
+// })
+
+// gallery.append(...items)
+
+// gallery.addEventListener('click', e => {
+//     e.preventDefault();
+//     if (e.target.nodeName !== 'IMG') {
+// 		return
+// 	}
+
+//     const selectedImage = e.target.getAttribute('data-source')
+
+//     const instance = basicLightbox.create(`
+//     <img src="${selectedImage}" width="800" height="600">
+// `)
+
+//     instance.show()
     
-    gallery.addEventListener('keydown', e => {
-		if (e.key === 'Escape') {
-			instance.close()
-		}
-	})
-})
+//     gallery.addEventListener('keydown', e => {
+// 		if (e.key === 'Escape') {
+// 			instance.close()
+// 		}
+// 	})
+// })
